0e1e34c53cbe75542081dccb768d74e20d9aa7d5
Kokiben <bennassarkaoutar@gmail.com>
2024-03-06 18:52:26 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,15 +20,20 @@ class FileStorage:
         FileStorage.__objects.update({key: obj})
 
     def save(self):
-    """Serializes __objects to the JSON file."""
-    serial_objects = {key_obj: value_obj.to_dict() for key_obj, value_obj in FileStorage.__objects.items()}
-    with open(FileStorage.__file_path, "w") as file_js:
-        json.dump(serial_objects, file_js, delimiter=4)
+        """Serializes __objects to the JSON file."""
+        serial_objects = {key_obj: value_obj.to_dict() for key_obj, value_obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w") as file_js:
+            json.dump(serial_objects, file_js, delimiter=4)
     
-     def reload(self):
-    """deserializes the JSON file to __objects."""
-    with open(FileStorage.__file_path, "r") as file_js:
-        filled_data = json.load(file_js)
-        for key_obj, dictionary_obj in filled_data.items():
-            family_class = dictionary_obj["__class__"]
-            FileStorage.__objects[key_obj] = eval(family_class)(**dictionary_obj)
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        try:
+           with open(FileStorage.__file_path, "r") as file_js:
+               filled_data = json.load(file_js)
+               for key_obj, dictionary_obj in filled_data.items():
+               family_class = dictionary_obj["__class__"]
+               FileStorage.__objects[key_obj] = eval(family_class)(**dictionary_obj)
+
+        except FileNotFoundError:
+            pass       
+
