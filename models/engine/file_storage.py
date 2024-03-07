dec362b30b6fee45c9ca2d97b9d1eb8cfe0c6031
Kokiben <bennassarkaoutar@gmail.com>
2024-03-06 16:13:31 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Print a class FileStorage that serializes instances to a JSON file and deserializes JSON file."""
+import os
 import json
 from models.base_model import BaseModel
 
@@ -12,6 +13,7 @@ class FileStorage:
     def all(self):
         """Return all objects saved in a dictionary."""
         return FileStorage.__objects
+
     def new(self, obj):
         """Include a new object to the repository."""
         key = f"{obj.__class__.__name__}.{obj.id}"
@@ -19,14 +21,14 @@ class FileStorage:
 
     def save(self):
     """Serializes __objects to the JSON file."""
-    serial_objects = {obj_id: obj.to_dict() for obj_id, obj in FileStorage.__objects.items()}
-    with open(FileStorage.__file_path, "w") as json_file:
-        json.dump(serial_objects, json_file, delimiter=2)
+    serial_objects = {key_obj: value_obj.to_dict() for key_obj, value_obj in FileStorage.__objects.items()}
+    with open(FileStorage.__file_path, "w") as file_js:
+        json.dump(serial_objects, file_js, delimiter=2)
     
      def reload(self):
     """deserializes the JSON file to __objects."""
-    with open(FileStorage.__file_path, "r") as json_file:
-        filled_data = json.load(json_file)
-        for obj_id, obj_dict in filled_data.items():
-            family_class = obj_dict["__class__"]
-            FileStorage.__objects[obj_id] = eval(family_class)(**obj_dict)
+    with open(FileStorage.__file_path, "r") as file_js:
+        filled_data = json.load(file_js)
+        for key_obj, id_obj in filled_data.items():
+            family_class = id_obj["__class__"]
+            FileStorage.__objects[key_obj] = eval(family_class)(**id_obj)
