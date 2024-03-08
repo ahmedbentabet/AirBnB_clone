49414f20bd3feb4595be65ac19ee897a7f9d46ff
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-03-08 22:04:06 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,11 +14,29 @@ class BaseModel:
         and sets the creation and update times.
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        # add this new instance to the FileStorage __objects
-        models.storage.new(self)
+        # convert "created_at" and "updated_at" : str -> datetime
+        if kwargs:
+            if "created_at" in kwargs:
+                kwargs["created_at"] = datetime.datetime.fromisoformat(
+                    kwargs["created_at"]
+                )
+            if "updated_at" in kwargs:
+                kwargs["updated_at"] = datetime.datetime.fromisoformat(
+                    kwargs["updated_at"]
+                )
+
+            # Initializes a new instance with the values from kwargs
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            # New instance with default values for id, created_at & updated_at.
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            # add this new instance to the FileStorage __objects
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance"""
@@ -39,7 +57,11 @@ class BaseModel:
         """
 
         my_dict = self.__dict__.copy()
+        # convert "created_at" and "updated_at" : datetime -> str
         my_dict['created_at'] = my_dict['created_at'].isoformat()
         my_dict['updated_at'] = my_dict['updated_at'].isoformat()
+
+        # add the class name to my_dict
         my_dict['__class__'] = self.__class__.__name__
+
         return my_dict
