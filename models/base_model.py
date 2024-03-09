8a65c9c6255deec98799149571db0dc9219c5242
ahmedbentabet <bentabet841@gmail.com>
2024-03-09 12:45:17 +0000

    Refactor base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """Define the Module base_module"""
-import models
-import models
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
     """Define BaseModel class"""
 
-    def __init__(self, *args, **kwargs):
     def __init__(self, *args, **kwargs):
         """
         Initializes the BaseModel instance with a unique ID,
@@ -19,11 +16,11 @@ class BaseModel:
         # convert "created_at" and "updated_at" : str -> datetime
         if kwargs:
             if "created_at" in kwargs:
-                kwargs["created_at"] = datetime.datetime.fromisoformat(
+                kwargs["created_at"] = datetime.fromisoformat(
                     kwargs["created_at"]
                 )
             if "updated_at" in kwargs:
-                kwargs["updated_at"] = datetime.datetime.fromisoformat(
+                kwargs["updated_at"] = datetime.fromisoformat(
                     kwargs["updated_at"]
                 )
 
@@ -34,11 +31,12 @@ class BaseModel:
 
         else:
             # New instance with default values for id, created_at & updated_at.
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             # add this new instance to the FileStorage __objects
-            # models.storage.new(self)
+            from models import storage  # Import here to avoid circular imports
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance"""
@@ -46,10 +44,15 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the 'updated_at' attribute to the current datetime."""
+        """
+        Updates the 'updated_at' attribute to the current datetime.
+        Calls the save method of the associated FileStorage instance to
+        serialize and store the current instance's state.
+        """
 
-        self.updated_at = datetime.datetime.now()
-        # models.storage.save()
+        self.updated_at = datetime.now()
+        from models import storage  # Import here to avoid circular imports
+        storage.save()
 
     def to_dict(self):
         """
@@ -65,9 +68,6 @@ class BaseModel:
         my_dict['updated_at'] = my_dict['updated_at'].isoformat()
 
         # add the class name to my_dict
-
-        # add the class name to my_dict
         my_dict['__class__'] = self.__class__.__name__
-
 
         return my_dict
