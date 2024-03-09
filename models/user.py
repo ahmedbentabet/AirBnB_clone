00b330618f500f8fab3194de4608cd3d49040355
ahmedbentabet <bentabet841@gmail.com>
2024-03-09 15:14:56 +0000

    Refactor user.py

--- models/user.py
+++ models/user.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Print a class User that inherits from BaseModel."""
+"""Define the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class attributes."""
+    """User class that inherits from BaseModel."""
 
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize User instance."""
+        super().__init__(*args, **kwargs)
