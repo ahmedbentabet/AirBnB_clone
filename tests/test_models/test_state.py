89c50a95a628d1e2d2445bd5dacaba31928cc9e4
Kokiben <bennassarkaoutar@gmail.com>
2024-03-09 19:38:09 -0800

    test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -14,7 +14,7 @@ class StateTestReport(unittest.TestCase):
         first_instance = State()
         self.assertEqual(State, type(State()))
         self.assertNotEqual(first_instance.id, State().id)
-        self.assertIsInstance(second_instnce, State)
+        self.assertIsInstance(second_instance, State)
 
     def test_state_creation_storage(self):
         """Verify the creation and storge of a new State instance."""
@@ -23,8 +23,8 @@ class StateTestReport(unittest.TestCase):
     def test_state_name(self):
         """Exmaine the name property of State objects."""
         first_instance = State()
-        self.assertIsInstance(first_instnce.name, str)
-        self.assertTrue(hasattr(first_instnce, "name"))
+        self.assertIsInstance(first_instance.name, str)
+        self.assertTrue(hasattr(first_instance, "name"))
         self.assertEqual(first_instance.name, "")
         # Asuming name initializes as an empty string
 
